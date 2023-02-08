d310917315a24f6a90d2029c9bab91d40407bd20
mirahy <myrahy@gmail.com>
2023-02-08 14:56:40 -0400

    incluindo route da midlleware permissao

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'authdev' => \App\Http\Middleware\AuthDev::class,
+        'permissao' => \App\Http\Middleware\Permissao::class,
     ];
 }
