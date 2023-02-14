a45eeab2c7a2813f2a8c44ce84184a27c3b7e164
Mirahy <myrahy@gmail.com>
2023-02-14 07:46:22 -0400

    Inserindo campos e relacionamentos do model  PeriodoLetivoCategoria

--- app/Models/PeriodoLetivoCategoria.php
+++ app/Models/PeriodoLetivoCategoria.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
@@ -15,11 +16,29 @@ class PeriodoLetivoCategoria extends Model implements Transformable
 {
     use TransformableTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [];
+    protected $fillable = [
+        'curso_id',
+        'periodo_letivo_id',
+        'categoria_id'
+    ];
+
+    protected $visible = [
+        'id',
+        'curso_id',
+        'periodo_letivo_id',
+        'categoria_id'
+    ];
+
+
+    public function curso()
+    {
+        return $this->belongsTo('App\Curso');
+    }
+
+    public function periodoLetivo()
+    {
+        return $this->belongsTo('App\PeriodoLetivo');
+    }
+
 
 }
