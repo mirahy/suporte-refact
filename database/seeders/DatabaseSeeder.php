9eff80293f582b7aa5b672c157f0ae0ef92bdd57
Mirahy <myrahy@gmail.com>
2023-02-14 07:47:04 -0400

    Configurando inserts via seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        $path = 'database/seeders/inserts_suporte_ead.sql';
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Database inserts completed successfully!');
+
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
